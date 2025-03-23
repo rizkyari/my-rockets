5d4f2552573b391f3a824c8be054d2d04fca6ee5
rizky.ariananda <rizky.ariananda@privyid.tech>
2025-03-23 11:11:56 +0700

    feat: delete function for custom rocket

--- src/components/rockets/RocketDetail.vue
+++ src/components/rockets/RocketDetail.vue
@@ -1,5 +1,13 @@
 <template>
     <v-card class="pa-4" :style="{backgroundColor: colors.space.card}">
+        <v-btn
+            v-if="isCustomRocket"
+            color="error"
+            class="my-4"
+            @click="confirmDialog = true"
+        >
+            Delete Rocket
+        </v-btn>
         <v-img :src="rocket.flickr_images[0]" height="300px" cover class="mb-4"/>
         <h2 class="text-h4 mb-2" :style="{color: colors.space.title}">
             {{ rocket.name }}
@@ -19,11 +27,39 @@
             {{ rocket.first_flight }}
         </p>
     </v-card>
+    <DeleteConfirmationDialog
+        v-model="confirmDialog"
+        title="Confirm Deletion"
+        message="Are you sure you want to delete this rocket?"
+        @confirm="deleteThisRocket"
+        @cancel="confirmDialog = false"
+    />
 </template>
 
 <script lang="ts" setup>
+import {ref,computed} from 'vue'
+import { useRouter } from 'vue-router';
+import { useRocketStore } from '@/store/rocketStore';
 import type { Rocket } from '@/types/rocket'
 import { colors } from '@/assets/color'
 
+import DeleteConfirmationDialog from '../ui/DeleteConfirmationDialog.vue';
+import router from '@/router';
+
 defineProps<{ rocket: Rocket}>()
+const store = useRocketStore()
+const roter = useRouter()
+
+const confirmDialog = ref(false)
+
+const isCustomRocket = computed(() =>
+    store.customRockets.some(r=> r.id === store.selectedRocket?.id)
+)
+
+const deleteThisRocket = () => {
+    if(!store.selectedRocket) return
+    store.deleteRocket(store.selectedRocket.id)
+    confirmDialog.value = false
+    router.push('/')
+}
 </script>
---
+++ src/components/ui/DeleteConfirmationDialog.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-dialog 
+        :model-value="modelValue"
+        @update:model-value="(val) => emit('update:modelValue', val)"
+        max-width="400"
+    >
+        <v-card>
+            <v-card-title class="text-h6">{{ title }}</v-card-title>
+            <v-card-text>{{ message }}</v-card-text>
+            <v-card-actions>
+                <v-spacer />
+                <v-btn color="grey" @click="$emit('cancel')">Cancel</v-btn>
+                <v-btn color="red" @click="$emit('confirm')">Delete</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+    modelValue: boolean
+    title?: string
+    message?: string
+}>()
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: boolean): void
+    (e: 'confirm'): void
+    (e: 'cancel'): void
+}>()
+</script>
